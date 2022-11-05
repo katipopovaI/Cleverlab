07586101fd88ccb3f51ac07ffe0a225e646fcad9
«katipopovaI» <katipopova@mail.ru>
2022-11-05 22:01:24 +0300

    change

--- JavaScript/homeworks_05/task3/script.js
+++ JavaScript/homeworks_05/task3/script.js
@@ -4,8 +4,6 @@ let num;
 do {
   num = prompt("Введите числа для вычисления суммы", "");
   num = Number(num);
-  console.log(num);
   sum += num;
-  console.log(sum);
 } while (num != 0);
 alert(`Сумма чисел:${sum}`);
--- JavaScript/homeworks_05/task4/script.js
+++ JavaScript/homeworks_05/task4/script.js
@@ -1,10 +1,7 @@
 "use strict";
 let sum = 0;
 let n = +prompt("Введите натуральное число", "");
-for (let i = 0; i < n; ) {
-  i++;
+for (let i = 0; i < n; i++) {
   sum += i;
-  console.log(i);
-  console.log(sum);
 }
 alert(`Сумма чисел=${sum}`);
